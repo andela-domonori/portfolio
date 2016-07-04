ff2331ba54516e9a94eee6c52346b206a59d55e4
Omonori Damilola Jesmine <damilola.omonori@andela.co>
2016-07-04 09:34:14 +0100

    styled overlay contents for portfolio grid

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,22 +5,45 @@ import {browserHistory} from 'history'
 
 class Portfolio extends Component {
   render() {
-    let list = ['Letterer', 'ChessMe', 'Scmobile']
+    let list = [
+        {
+          name: 'Letterer',
+          caption: 'A game that will strecth your mind'
+        },
+        {
+          name: 'ChessMe',
+          caption: 'Learn to play with the grandmasters'
+        },
+        {
+          name: 'Scmobile',
+          caption: 'A mobile app for the saints community church to get latest messages and the likes'
+        }
+    ]
     return (
       <div>
-        <Grid  fluid>
+        <Grid fluid>
           <Row>
             {
               list.map((item, index) => (
                 <Col
                   key={index}
-                  className={`portfotio-grid-${index}`} md={4}>
-                  <div className="class-with-pad">
-                    <img src={require("./../../images/jesmine.jpg")} />
-                    <div className="portfolio-overlay">{item}</div>
-                  </div>
+                  className={`portfotio-grid-${index} no-padding`} md={4}>
+                  <Col md={12}>
+                    <div className="class-with-pad">
+                      <img src={require("./../../images/jesmine.jpg")} />
+                      <div className="portfolio-overlay">
+                        <div id="overlay-caption">
+                          <h4>{item.name}</h4>
+                          <p>{item.caption}</p>
+                        </div>
+                        <p id="overlay-content">
+                          To play the game move the alphabets one after the other. The winner wins
+                        </p>
+                      </div>
+                    </div>
+                  </Col>
                 </Col> )
-                )
+              )
             }
           </Row>
         </Grid>
